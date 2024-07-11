ccc29fc42c12e45590faa5f4a97244211680d3d9
rapritchard <rapritchard96@gmail.com>
2024-07-11 16:09:15 +0100

    feat: add ch2.c1 lesson

---
+++ src/ch2/ch2.c1.py
@@ -0,0 +1,20 @@
+""" 
+EXPONENTIAL DECAY
+In physics, exponential decay is a process where a quantity decreases over time at a rate proportional to its current value.
+
+We've found that Instagram influencers tend to lose followers similarly. This means that the more followers you have, the faster you lose them.
+
+ASSIGNMENT
+Complete the decayed_followers function.
+
+It calculates the final value of a quantity after a certain time has passed, given its initial value and a rate of decay. Return the remaining followers.
+
+remaining_total = quantity * ( retention_rate ^ time )
+
+The retention_rate is the opposite of fraction_lost_daily. 
+If an influencer lost 0.2 (or 20%) of their followers each day, then the retention rate would be 0.8 (or 80%). 
+"""
+
+def decayed_followers(intl_followers, fraction_lost_daily, days):
+    retention_rate = float(1 - fraction_lost_daily)
+    return intl_followers * (retention_rate ** days)
--- src/ch2/ch2.p11.py
+++ src/ch2/ch2.p11.py
@@ -3,7 +3,7 @@ FACTORIALS
 In math, the factorial of a positive integer n, written n!, is the product of all positive integers less than and equal to n.
 
 5! = 5 * 4 * 3 * 2 * 1
-Copy icon
+
 The results of a factorial grow even faster than exponentiation!
 
 n! grows faster than 2^n:
@@ -27,3 +27,4 @@ def num_possible_orders(num_posts):
     for num in range(2, num_posts + 1):
         possible_orders *= num
     return possible_orders
+
